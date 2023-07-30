c34d3fd3389e22e5556038c7234ba34166d4a6d3
ramblehead <v.rybynok@gmail.com>
2023-07-30 17:36:05 +0100

    Checkpoint

--- rh-template/expand.py
+++ rh-template/expand.py
@@ -2,14 +2,12 @@
 # Hey Emacs, this is -*- coding: utf-8; mode: python -*-
 
 
-# from mako.lookup import TemplateLookup
-
-# from . import conf
-
 import importlib.util
 from pathlib import Path
 from types import ModuleType
 from typing import Self
+
+from mako.lookup import TemplateLookup  # type: ignore reportMissingTypeStubs
 
 _sd_path = (Path(__file__).parent / ".").resolve(strict=True)
 
@@ -30,29 +28,26 @@ def import_module_from_file(module_path: Path, module_name: str) -> ModuleType:
 
 conf = import_module_from_file(_sd_path / "conf.py", "conf")
 
-# def expand_content(template_in_path: Path, file_out_path: Path) -> None:
-#     template_lookup = TemplateLookup(directories=[template_in_path.parent])
-#     template = template_lookup.get_template(template_in_path.name)
-#     file_out_str: str = template.render(conf=conf)
 
-#     try:
-#         with Path.open(file_out_path, "w") as file:
-#             file.write(file_out_str)
-#         print("String successfully written to", file_out_path)
-#     except OSError as cause:
-#         print("Error writing to file:", str(cause))
+def expand_content(in_template_path: Path, out_file_path: Path) -> None:
+    template_lookup = TemplateLookup(directories=[in_template_path.parent])
+    template = template_lookup.get_template(in_template_path.name)  # type: ignore unknownMemberType
+    file_out_str: str = template.render(conf=conf)  # type: ignore unknownMemberType
+
+    try:
+        with Path.open(out_file_path, "w") as file:
+            file.write(file_out_str)
+        print("String successfully written to", out_file_path)
+    except OSError as cause:
+        print("Error writing to file:", str(cause))
 
 
-# template_in_path = (
-#     Path(__file__).parent.resolve(strict=True) / ".rh-project.rename" / "init.el.mako"
-# )
+template_in_path = (
+    _sd_path.parent.resolve(strict=True) / ".rh-project.rename" / "init.el.mako"
+)
 
-# file_out_path = (
-#     Path(__file__).parent.resolve(strict=True) / ".rh-project.rename" / "init.el"
-# )
+file_out_path = _sd_path.parent.resolve(strict=True) / ".rh-project.rename" / "init.el"
 
-# print(template_in_path, file_out_path)
+print(template_in_path, file_out_path)
 
-# expand_content(template_in_path, file_out_path)
-
-print("xxx", conf.project_name)
+expand_content(template_in_path, file_out_path)

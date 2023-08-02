1bb2f7ec31664e5d3905de2d8bdd3b813005bb0c
ramblehead <v.rybynok@gmail.com>
2023-08-02 20:14:10 +0100

    Checkpoint

--- rh_template/utils.py
+++ rh_template/utils.py
@@ -7,12 +7,16 @@ from typing import cast
 def make_words(*human_names: str) -> list[str]:
     result: list[str] = []
     for human_name in human_names:
-        result += [word.lower() for word in cast(str, re.split(r" |-", human_name))]
+        result += [word.lower() for word in cast(str, re.split(r" |-|_", human_name))]
     return result
 
 
 def kebab_case(*human_names: str) -> str:
     return "-".join(make_words(*human_names))
+
+
+def snake_case(*human_names: str) -> str:
+    return "_".join(make_words(*human_names))
 
 
 def upcase_initial(s: str) -> str:
